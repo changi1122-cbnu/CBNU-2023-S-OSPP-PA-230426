77d2425e403b3d60e2ee2f4d695161268e891662
Woochang Lee <113578987+changi1122-cbnu@users.noreply.github.com>
2023-04-27 11:12:22 +0900

    Initial commit

---
+++ P12_1_NaverDatabase/app/src/main/java/net/studio1122/p12_1_naverdatabase/MainActivity.java
@@ -0,0 +1,14 @@
+package net.studio1122.p12_1_naverdatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
